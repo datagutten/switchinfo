a1271d9e2ef45471fe0be74a32f86c872f990b42
datagutten <datagutten@datagutten.net>
2018-12-04 09:29:32 +0100

    Add path to update_all.sh

--- update_all.sh
+++ update_all.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-python3 manage.py load_vlans all
-python3 manage.py load_interfaces_rfc all
-python3 manage.py load_mac all
+# https://kvz.io/blog/2013/11/21/bash-best-practices/
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+python3 ${__dir}/manage.py load_vlans all
+python3 ${__dir}/manage.py load_interfaces_rfc all
+python3 ${__dir}/manage.py load_mac all
