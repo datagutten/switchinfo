972359a16cc08ecb386f1f13bea647e4906af553
datagutten <datagutten@datagutten.net>
2019-11-29 10:32:24 +0100

    Load OUI directly from ieee.org

--- management/commands/load_oui.py
+++ management/commands/load_oui.py
@@ -1,27 +1,21 @@
-from django.core.management.base import BaseCommand  # , CommandError
-# from django.utils import timezone
+import requests
+from django.core.management.base import BaseCommand
 
 from switchinfo.models import Oui
 
-
-from pprint import pprint
 
 class Command(BaseCommand):
     help = 'Import OUI'
 
     def handle(self, *args, **options):
         import re
-        from pprint import pprint
-        file = open ('/home/switch_info/switchinfo/oui.txt', 'r')
-        oui = file.read()
+        response = requests.get('http://standards-oui.ieee.org/oui.txt')
 
-        pattern = '([A-F0-9]{6})\s+\(.+\)\s+(.+)'
-        matches = re.findall(pattern, oui)
+        pattern = r'([A-F0-9]{6})\s+\(.+\)\s+(.+)'
+        matches = re.findall(pattern, response.text)
 
         for vendor in matches:
-            #pprint (vendor[0])
             Oui.objects.get_or_create(
                     prefix=vendor[0],
-                    defaults = {'vendor': vendor[1]},
+                    defaults={'vendor': vendor[1]},
                     )
-            #break
