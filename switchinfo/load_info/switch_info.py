8fa41e8dbc91a0d1ecacdaf634628453c28a515a
datagutten <datagutten@datagutten.net>
2019-12-03 12:33:40 +0100

    Improve model detection and detect series

--- switchinfo/load_info/switch_info.py
+++ switchinfo/load_info/switch_info.py
@@ -19,14 +19,18 @@ def switch_info(ip=None, community=None, device=None):
     pprint(info)
     switch.community = community
     switch.type = switch_type(info['descr'])
-    print(switch.type)
+    print('Type: %s' % switch.type)
     switch.name = info['name'].split('.')[0]
     switch.description = info['descr']
 
-    if info['model'] == '':
-        switch.model = model_from_description(device, switch.type)
-    else:
+    if 'model' in info and not info['model'] == '':
         switch.model = info['model']
+    else:
+        switch.model = model_from_description(device, switch.type)
+
+    switch.series = switch_series(switch)
+
+    print('Series: %s' % switch.series)
     print(switch.model)
     switch.save()
     return switch
@@ -45,15 +49,28 @@ def switch_type(descr):
         return 'Westermo'
 
 
+def switch_series(switch):
+    series = None
+    if switch.type == 'Cisco':
+        series = re.match(r'((?:WS|IE)-[A-Z0-9]+).*?$', switch.model)
+    elif switch.type == 'Aruba':
+        series = re.match(r'Aruba [A-Z0-9]+ ([A-Z0-9]+)', switch.description)
 
-def model_from_description(device, switch_type):
-    descr = device.create_list(oid='.1.3.6.1.2.1.47.1.1.1.1.2')  # ENTITY-MIB::entPhysicalDescr
-    for string in descr:
-        if switch_type == 'Aruba':
+    if series:
+        return series.group(1)
+
+
+def model_from_description(description=None, device=None):
+    if not description:
+        description = device.create_list(oid='.1.3.6.1.2.1.47.1.1.1.1.2')  # ENTITY-MIB::entPhysicalDescr
+    sw_type = switch_type(description)
+
+    for string in description:
+        if sw_type == 'Aruba':
             matches = re.match(r'Aruba [A-Z0-9]+\s(.+)\sSwitch', string)
-        elif switch_type == 'Cisco':
-            matches = re.match(r'Cisco.+?((:WS\-)?[cC][A-Z0-9\-]+).*switch', string)
-        elif switch_type == 'HP':
+        elif sw_type == 'Cisco':
+            matches = re.match(r'Cisco.+?((:WS\-)?[cC][A-Z0-9\-]+).*switch', string, flags=re.DOTALL)
+        elif sw_type == 'HP':
             matches = re.match(r'HP ([0-9\-]+) Switch Software', string)
         else:
             return None  # Unsupported type
