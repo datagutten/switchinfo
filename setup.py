f8543603917e69fcea3771bde0c5cc6341bf86c6
datagutten <datagutten@datagutten.net>
2020-02-15 20:14:53 +0100

    Add git tag as version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='switchinfo',
-    version='1.1',
+    version='@@version@@',
     packages=find_packages(),
     include_package_data=True,
     license='GPL',
