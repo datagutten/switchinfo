b25f6aa00b1ad153912f01fa03ea82140b8e0cb1
datagutten <datagutten@datagutten.net>
2020-02-16 13:57:36 +0100

    Run tests with both easysnmp and netsnmp

--- runtests.py
+++ runtests.py
@@ -9,6 +9,11 @@ from django.test.utils import get_runner
 if __name__ == "__main__":
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
     django.setup()
+    if os.environ['USE_NETSNMP'] == 'False':
+        settings.USE_NETSNMP = False
+    else:
+        settings.USE_NETSNMP = True
+
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
     failures = test_runner.run_tests(["tests.tests_cisco_snmp",
