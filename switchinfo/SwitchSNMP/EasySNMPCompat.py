2f89f59a64aa02e7311ec0dba7668db410705a44
datagutten <datagutten@datagutten.net>
2022-01-04 11:57:25 +0100

    Add close and walk methods to EasySNMPCompat

--- switchinfo/SwitchSNMP/EasySNMPCompat.py
+++ switchinfo/SwitchSNMP/EasySNMPCompat.py
@@ -13,6 +13,9 @@ class EasySNMPCompat(easysnmp.Session):
         except SystemError as e:
             raise exceptions.SNMPError(e, self)
 
+    def close(self):
+        pass
+
     def get(self, oids):
         try:
             return super().get(oids)
@@ -25,3 +28,14 @@ class EasySNMPCompat(easysnmp.Session):
         except SystemError as e:
             raise exceptions.SNMPError(e, self, oids)
 
+    def walk(self, oids='.1.3.6.1.2.1'):
+        try:
+            return super().walk(oids)
+        except easysnmp.exceptions.EasySNMPTimeoutError as e:
+            raise exceptions.SNMPTimeout(e, self, oids)
+        except easysnmp.exceptions.EasySNMPConnectionError as e:
+            raise exceptions.SNMPConnectionError(e, self, oids)
+        except easysnmp.exceptions.EasySNMPNoSuchInstanceError as e:
+            raise exceptions.SNMPNoData(e, self, oids)
+        except SystemError as e:
+            raise exceptions.SNMPError(e, self, oids)
