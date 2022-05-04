fdb986f1995af96411e8539a6b2f4ca179e65733
datagutten <datagutten@datagutten.net>
2022-05-04 14:25:57 +0200

    Add neighbor search to form

--- switchinfo/forms.py
+++ switchinfo/forms.py
@@ -4,3 +4,4 @@ from django import forms
 class SearchForm(forms.Form):
     ip = forms.CharField(label='IP', required=False)
     mac = forms.CharField(label='MAC', required=False)
+    neighbor = forms.CharField(label='CDP/LLDP neighbor', required=False)
