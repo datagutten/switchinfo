bfef1a656738481edd24115ebf05e68056b98e9b
datagutten <datagutten@datagutten.net>
2023-02-10 21:29:04 +0100

    Force timeout to int when using pynetsnmp

--- switchinfo/SwitchSNMP/pynetsnmpCompat.py
+++ switchinfo/SwitchSNMP/pynetsnmpCompat.py
@@ -3,9 +3,9 @@ from pynetsnmp import netsnmp
 
 
 class PynetsnmpCompat(pynetsnmp.netsnmp.Session):
-    def __init__(self, hostname, community, version=2, timeout=0.5, retries=1):
+    def __init__(self, hostname, community, version=2, timeout=1, retries=1):
         super().__init__(version=version,
-                         timeout=timeout,
+                         timeout=int(timeout),
                          retries=int(retries - 1),
                          peername=hostname,
                          community=community,
