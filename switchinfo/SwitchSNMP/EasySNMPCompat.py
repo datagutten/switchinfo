a58911bb10a93add9daef2e874ef865873cb77dd
datagutten <datagutten@datagutten.net>
2023-05-15 21:00:01 +0200

    Use custom SNMPVariable class with easysnmp

--- switchinfo/SwitchSNMP/EasySNMPCompat.py
+++ switchinfo/SwitchSNMP/EasySNMPCompat.py
@@ -1,5 +1,7 @@
 import easysnmp
-from . import exceptions
+from easysnmp import SNMPVariable as EasySNMPVariable
+
+from . import exceptions, SNMPVariable as CustomSNMPVariable
 
 
 class EasySNMPCompat(easysnmp.Session):
@@ -18,7 +20,7 @@ class EasySNMPCompat(easysnmp.Session):
 
     def get(self, oids):
         try:
-            return super().get(oids)
+            return convert_variable(super().get(oids))
         except easysnmp.exceptions.EasySNMPTimeoutError as e:
             raise exceptions.SNMPTimeout(e, self, oids)
         except easysnmp.exceptions.EasySNMPConnectionError as e:
@@ -30,7 +32,7 @@ class EasySNMPCompat(easysnmp.Session):
 
     def walk(self, oids='.1.3.6.1.2.1'):
         try:
-            return super().walk(oids)
+            return list(map(lambda var: convert_variable(var), super().walk(oids)))
         except easysnmp.exceptions.EasySNMPTimeoutError as e:
             raise exceptions.SNMPTimeout(e, self, oids)
         except easysnmp.exceptions.EasySNMPConnectionError as e:
@@ -39,3 +41,7 @@ class EasySNMPCompat(easysnmp.Session):
             raise exceptions.SNMPNoData(e, self, oids)
         except SystemError as e:
             raise exceptions.SNMPError(e, self, oids)
+
+
+def convert_variable(var: EasySNMPVariable):
+    return CustomSNMPVariable(oid=var.oid, oid_index=var.oid_index, value=var.value, snmp_type=var.snmp_type)
