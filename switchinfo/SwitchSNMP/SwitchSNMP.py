datagutten/switchinfo

commit ef8d324066187397e535251247e7f26314c9914e

Keep original key type

switchinfo/SwitchSNMP/SwitchSNMP.py
@@ -306,6 +306,7 @@ class SwitchSNMP:
             6: 'otherFault'
         }
         for key, value in ports.items():
+            # noinspection PyTypeChecker
             ports[key]['pethPsePortDetectionStatus'] = states[value['pethPsePortDetectionStatus']]
 
         return ports
@@ -484,7 +485,7 @@ class SwitchSNMP:
                     keys[key_names[key_num]] = sub_key
                 else:
                     if key_num == len(key_names):
-                        keys[key_names[-1]] = str(sub_key)
+                        keys[key_names[-1]] = sub_key
                     else:
                         keys[key_names[-1]] += '.%d' % sub_key
                 key_num += 1

commit 95ac72b6ca5f7b2cee31609f21eccbb9c1894473

Force key to be string when combining keys (broken in ef8d324066187397e535251247e7f26314c9914e)

switchinfo/SwitchSNMP/SwitchSNMP.py
@@ -487,7 +487,7 @@ class SwitchSNMP:
                     if key_num == len(key_names):
                         keys[key_names[-1]] = sub_key
                     else:
-                        keys[key_names[-1]] += '.%d' % sub_key
+                        keys[key_names[-1]] = '%s.%d' % (str(keys[key_names[-1]]), sub_key)
                 key_num += 1
 
             if join_keys: