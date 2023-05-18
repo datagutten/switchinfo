0b437ff62804454e47f194c03902943e25d35242
datagutten <datagutten@datagutten.net>
2023-05-18 14:58:17 +0200

    Run Cisco SNMP tests with plain unittest

--- tests_unittest/SwitchSNMP/test_CiscoIOS.py
+++ tests_unittest/SwitchSNMP/test_CiscoIOS.py
@@ -13,11 +13,52 @@ class CiscoIOSTestCase(unittest.TestCase):
         key = list(poe.keys())[0]
         self.assertEqual('1', key)
 
-    def test_cisco(self):
+    def test_lldp(self):
         lldp = session_lldp.lldp()
         self.assertEqual(lldp[4][0]['device_id'], 'SFIKT-34:d2')
         self.assertEqual(lldp[4][0]['local_port_num'], 4)
         self.assertEqual(lldp[4][0]['remote_port'], '3817c3c934d2')
+        
+    def test_switch_info_cisco(self):
+        info_cisco = session.switch_info()
+        self.assertEqual('WS-C2960S-24PS-L', info_cisco['model'])
+        self.assertEqual('ROV-SW-01.switch.ltf.local', info_cisco['name'])
+        self.assertIn('Cisco IOS Software, C2960S Software', info_cisco['descr'])
+
+    def test_vlans(self):
+        self.assertEqual([1, 9, 12, 100], session.vlans())
+
+    def test_interfaces(self):
+        interfaces = session.interfaces_rfc()
+        self.assertEqual('Trunk serverswitch', interfaces['alias']['10124'])
+        self.assertEqual('GigabitEthernet1/0/24', interfaces['descr']['10124'])
+
+    def test_mac_on_port(self):
+        mac = session.mac_on_port()
+        self.assertIn('00254532caae', mac)
+        self.assertEqual('232', mac['00254532caae'])
+
+    def test_bridgePort_to_ifIndex(self):
+        table = session.bridgePort_to_ifIndex()
+        self.assertEqual('10124', table['24'])
+
+    def test_interface_poe_status(self):
+        status = session.interface_poe_status()
+        self.assertEqual('disabled', status['10'])
+        self.assertEqual('deliveringPower', status['22'])
+
+    def test_vlan_names(self):
+        names = session.vlan_names()
+        expected_vlans = {1: 'default', 9: 'Modem', 12: 'PC',
+                          100: 'Management', 1002: 'fddi-default',
+                          1003: 'token-ring-default',
+                          1004: 'fddinet-default', 1005: 'trnet-default'}
+        self.assertEqual(expected_vlans.items(), names.items())
+
+    def test_vlan_ports(self):
+        ports = session.vlan_ports()
+        self.assertEqual(12, ports[2][10101])
+        self.assertEqual(1, ports[2][10127])
 
 
 if __name__ == "__main__":
