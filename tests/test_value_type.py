4b9e4c6f1b157b17c1bc3f576edf6e7a249f109b
datagutten <datagutten@datagutten.net>
2024-06-16 22:46:14 +0200

    Add test for MAC-address conversion

--- tests/test_value_type.py
+++ tests/test_value_type.py
@@ -1,10 +1,12 @@
-import os
 import unittest
 
 from parameterized import parameterized
 
+from switchinfo.SwitchSNMP import mibs, utils
+from switchinfo.SwitchSNMP.EasySNMPCompat import EasySNMPCompat
+from switchinfo.SwitchSNMP.NetSNMPCompat import NetSNMPCompat
 from switchinfo.SwitchSNMP.SwitchSNMP import SwitchSNMP, select_library
-from switchinfo.SwitchSNMP import mibs
+
 
 def build_combinations(libraries, vendors):
     combinations = []
@@ -35,6 +37,17 @@ class SNMPValueTypeTestCase(unittest.TestCase):
         self.assertEqual(int, type(interfaces[interface_key]['ifHighSpeed']))
         self.assertEqual(str, type(interfaces[interface_key]['ifAlias']))
         self.assertEqual(int, type(interfaces[interface_key]['ifInMulticastPkts']))
+
+    @parameterized.expand([['netsnmp', 1], ['easysnmp', 2]])
+    def test_mac(self, snmp_library, key):
+        session_lldp = SwitchSNMP('lldp_cisco', '127.0.0.%d' % key, snmp_library=snmp_library)
+        if snmp_library == 'netsnmp':
+            self.assertIsInstance(session_lldp.get_session(), NetSNMPCompat)
+        elif snmp_library == 'easysnmp':
+            self.assertIsInstance(session_lldp.get_session(), EasySNMPCompat)
+        mac = session_lldp.get_session().get('.1.0.8802.1.1.2.1.4.1.1.5.0.4.803')
+
+        self.assertEqual('3817c3c934d2', utils.mac_string(mac.typed_value()))
 
 
 if __name__ == "__main__":
