86b8faecca2f31b1d098c43722136a8e131bf88f
Anders Birkenes <datagutten@datagutten.net>
2025-07-29 17:23:47 +0200

    Fix ARP loading for Aruba AOS-S

--- switchinfo/SwitchSNMP/ArubaVSF.py
+++ switchinfo/SwitchSNMP/ArubaVSF.py
@@ -2,6 +2,8 @@ from switchinfo.SwitchSNMP.SwitchSNMP import SwitchSNMP
 
 
 class ArubaVSF(SwitchSNMP):
+    arp_oid = 'ipNetToMediaPhysAddress'
+
     def stack_ports(self):
         oid = '.1.3.6.1.4.1.11.2.14.11.5.1.116.1.5.1.2'
         return self.create_dict(oid=oid, int_index=True)
