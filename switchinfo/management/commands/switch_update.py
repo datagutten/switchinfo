6efdca3749c54dfaeeb2c355808548a0f82abf11
Anders Birkenes <datagutten@datagutten.net>
2025-11-16 08:49:25 +0100

    Rename backup when switch is renamed

--- switchinfo/management/commands/switch_update.py
+++ switchinfo/management/commands/switch_update.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 import switchinfo.load_info.switch_info as switch_info
 from snmp_compat import snmp_exceptions
 from switchinfo.SwitchSNMP.select import get_switch
@@ -8,12 +10,21 @@ class Command(SwitchBaseCommand):
     help = 'Update info about a switch'
 
     def handle(self, *args, **options):
+        try:
+            from config_backup.git import Git
+            git = Git(settings.BACKUP_PATH)
+        except ImportError:
+            git = None
 
         for switch in self.handle_arguments(options):
             print(switch)
             try:
                 device = get_switch(switch)
-                print(switch_info.switch_info(device=device))
+                switch_new = switch_info.switch_info(device=device)
+                print(switch_new)
+                if switch_new.name != switch.name and git:
+                    git.move(switch.name, switch_new.name)
+
             except snmp_exceptions.SNMPError as e:
                 print(switch, e)
                 continue
