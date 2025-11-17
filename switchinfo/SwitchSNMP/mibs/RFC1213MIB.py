147c96ced1c9ccd1b691cebdcda7c3100f85a005
Anders Birkenes <datagutten@datagutten.net>
2025-11-17 20:21:55 +0100

    Format MAC-address correct

--- switchinfo/SwitchSNMP/mibs/RFC1213MIB.py
+++ switchinfo/SwitchSNMP/mibs/RFC1213MIB.py
@@ -1,12 +1,33 @@
 from .SNMPMib import SNMPMib
+from .. import utils
 
 
 # noinspection PyPep8Naming
 class RFC1213MIB(SNMPMib):
     def ipNetToMediaTable(self, field_filter: list = None):
-        return self.snmp.snmp_table('.1.3.6.1.2.1.4.22.1', {
+        session = self.snmp.get_session()
+        oid = '.1.3.6.1.2.1.4.22.1'
+        key_mappings = {
             1: 'ipNetToMediaIfIndex',
             2: 'ipNetToMediaPhysAddress',
             3: 'ipNetToMediaNetAddress',
             4: 'ipNetToMediaType',
-        }, field_filter)
+        }
+        data = {}
+
+        for item in session.bulkwalk(oid):
+            row, col = utils.table_index(oid, item.oid)
+
+            try:
+                field_name = key_mappings[col]
+            except KeyError:
+                field_name = col
+
+            if row not in data.keys():
+                data[row] = {}
+            if field_name == 'ipNetToMediaPhysAddress':
+                data[row][field_name] = item.hex_string()
+            else:
+                data[row][field_name] = item.typed_value()
+
+        return data
