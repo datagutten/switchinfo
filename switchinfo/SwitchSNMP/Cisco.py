0432dd4076d8c7b1984fa1bcade42ce440c1b10b
Anders Birkenes <datagutten@datagutten.net>
2025-11-18 21:50:10 +0100

    Fix tagged_vlans for Cisco

--- switchinfo/SwitchSNMP/Cisco.py
+++ switchinfo/SwitchSNMP/Cisco.py
@@ -65,7 +65,7 @@ class Cisco(SwitchSNMP, ABC):
     def tagged_vlans(self):
         # CISCO-VTP-MIB::vlanTrunkPortVlansEnabled
         oid = '.1.3.6.1.4.1.9.9.46.1.6.1.1.4'
-        return self.create_dict(oid=oid, int_index=True)
+        return self.create_dict(oid=oid, int_index=True, typed_value=False)
 
     def vlan_ports(self, debug=False, **kwargs):
         # vtp_mib = mibs.CiscoVTP(self)
